62c0110b70c271e482f5a22f0291e5e6f53f5361
bilal <bilalmustofa146@gmail.com>
2026-04-15 15:21:51 -0600

    added some script

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ const calculateBtn = document.getElementById("calculateBtn");
 // calculate the age when click button 
 calculateBtn.addEventListener('click', () => {
     const birthDataValue = document.getElementById("birthData").value;
+    const result = document.getElementById('result');
+    // First check input
+    if(birthDataValue === "") {
+        result.innerText = `Please, Enter your birthdate`;
+        return;
+    } 
     const birthdate = new Date(birthDataValue);
     const today = new Date();
     let age = today.getFullYear() - birthdate.getFullYear();
@@ -9,9 +15,13 @@ calculateBtn.addEventListener('click', () => {
     
     if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthdate.getDate())){
         age--;
-    }
+    } 
+    // check input is not future date
+    if (birthdate > today) {
+    result.innerText = "Birthdate can't be in the future!";
+    return;
+}
 
     // Display the result
-    const result = document.getElementById('result');
-    result.innerText = `Your Age is ${age} years old.`;
+        result.innerText = `Your Age is ${age} years old.`;
 });
