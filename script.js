f1ce86b781df76d0b049e79f9e6a5797db23725a
bilal <bilalmustofa146@gmail.com>
2026-04-20 13:50:54 -0600

    added full date of birth JS function

--- script.js
+++ script.js
@@ -1,27 +1,50 @@
 const calculateBtn = document.getElementById("calculateBtn");
+const userInput = document.getElementById("birthDate");
+userInput.max = new Date().toISOString().split('T')[0]; // is not allow for user to insert future dob
+
 // calculate the age when click button 
 calculateBtn.addEventListener('click', () => {
-    const birthDataValue = document.getElementById("birthData").value;
-    const result = document.getElementById('result');
-    // First check input
-    if(birthDataValue === "") {
-        result.innerText = `Please, Enter your birthdate`;
-        return;
-    } 
-    const birthdate = new Date(birthDataValue);
-    const today = new Date();
-    let age = today.getFullYear() - birthdate.getFullYear();
-    const monthDifference = today.getMonth() - birthdate.getMonth();
-    
-    if (monthDifference < 0 || (monthDifference === 0 && today.getDate() < birthdate.getDate())){
-        age--;
-    } 
-    // check input is not future date
-    if (birthdate > today) {
-    result.innerText = "Birthdate can't be in the future!";
-    return;
-}
+    const birthdate = new Date(userInput.value);
 
+    const dayBirthdate = birthdate.getDate() + 1;
+    const monthBirthdate = birthdate.getMonth() + 1;
+    const yearBirthdate = birthdate.getFullYear();
+
+    const today = new Date();
+    
+    const dayToday = today.getDate();
+    const monthToday = today.getMonth() + 1;
+    const yearToday = today.getFullYear();
+
+    let dayDifference, monthDifference, yearDifference;
+
+    yearDifference = yearToday - yearBirthdate;
+
+    if(monthToday >= monthBirthdate){
+        monthDifference = monthToday - monthBirthdate;
+    } else {
+        yearDifference--;
+        monthDifference = 12 + monthToday - monthBirthdate;
+    }
+
+    if(dayToday >= dayBirthdate){
+        dayDifference = dayToday - dayBirthdate;
+    } else {
+        monthDifference--;
+        dayDifference = getDaysInMonth(yearBirthdate, monthBirthdate) + dayToday - dayBirthdate;
+    }
+
+    if(monthDifference < 0){
+        monthDifference = 11;
+        yearDifference--;
+    }
+
+    
     // Display the result
-        result.innerText = `Your Age is ${age} years old.`;
+    const result = document.getElementById('result');
+    result.innerText = `Your Age is ${yearDifference} Years, ${monthDifference} Months and ${dayDifference} Days.`;
 });
+
+function getDaysInMonth(year, month){
+    return new Date(year, month, 0).getDate();
+}
